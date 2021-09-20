a111f71ad3c74a576b20774b23a3dc6069f593d0
dmunoz <dmunoz@host.docker.internal>
2021-09-20 11:16:54 -0500

    First Commit

---
+++ codeCommitDemo/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello/{name}")
+	public String sayHello(@PathVariable String name) {
+		return "Hi from CodeCommitDemo - v1 " + name;
+	}
+}
